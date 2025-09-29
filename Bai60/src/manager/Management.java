6a5868e10a0958d222d11969c343cb36465ea5a6
tuanfptu122005194908 <caothanhtuan576@gmail.com>
2025-09-29 23:58:37 +0700

    update bai 60

--- Bai60/src/manager/Management.java
+++ Bai60/src/manager/Management.java
@@ -6,9 +6,28 @@ import object.Bill;
 
 public class Management {
 
+    public void printResult(int total, boolean canPay) {
+        // in kết quả
+        System.out.println("this is total bill: " + total);
+        if (canPay) {
+            System.out.println("You can buy it.");
+        } else {
+            System.out.println("You can't buy it.");
+        }
+    }
+
+    // tinh tong
+    public int calc(Person p) {
+        return p.getBill().calcTotal();
+    }
+
+    // kiem tra thanh toan
+    public boolean check(Person p, int total) {
+        return p.getWallet().payMoney(total);
+    }
+
     // chạy chương trình
-    public void run() {
-        System.out.println("======= Shopping program =======");
+    public Person inputData() {
         int n = Validation.checkInput("input number of bill: ");
 
         int[] billValues = new int[n];
@@ -21,18 +40,19 @@ public class Management {
         // tạo đối tượng
         Bill bill = new Bill(billValues);
         Wallet wallet = new Wallet(w);
-        Person p = new Person(wallet, bill);
+        return new Person(wallet, bill);
 
-        // tính tổng và kiểm tra
-        int total = p.getBill().calcTotal();
-        boolean canPay = p.getWallet().payMoney(total);
+    }
 
-        // in kết quả
-        System.out.println("this is total bill: " + total);
-        if (canPay) {
-            System.out.println("You can buy it.");
-        } else {
-            System.out.println("You can't buy it.");
-        }
+    public void run() {
+        System.out.println("======= Shopping program =======");
+
+        Person p = inputData();
+
+        int total = calc(p);
+        
+        boolean canPay = check(p, total);
+        
+        printResult(total, canPay);
     }
 }
